c4fa5a671adf5448120e53275145120b5960f536
davidjagga <djjagga@gmail.com>
2024-12-23 22:09:57 -0500

    added test routes to localize issue, removing rides, user

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.json());
 
 
-const ridesRoutes = require('./api/ride');
-const userRoutes = require('./api/user');
+// const ridesRoutes = require('./api/ride');
+// const userRoutes = require('./api/user');
 const testRoutes = require('./api/test');
 
 
