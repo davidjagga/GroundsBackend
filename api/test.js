bbfbb23880b2942eefda4fe924f1d19177392125
davidjagga <djjagga@gmail.com>
2024-12-23 22:24:18 -0500

    removed Approval

--- api/test.js
+++ api/test.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { PrismaClient, Approval } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
